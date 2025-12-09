ae7fc754e5af740d56aa5c7b82b3b54a8bf09d01
coره <longfei1bot@gmail.com>
2025-12-10 01:33:02 +0800

    support katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,16 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from "./src/consts";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
         light: "catppuccin-latte",
