5ef73b55cf272fef02bbfe45b9ff6802f0de29cd
lessmore <leismo7@gmail.com>
2026-07-30 21:10:58 +0800

    fix: refine Mermaid image rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,10 +33,6 @@ export default defineConfig({
             securityLevel: "strict",
             theme: "default",
           },
-          dark: {
-            securityLevel: "strict",
-            theme: "dark",
-          },
         },
       ],
     ],
